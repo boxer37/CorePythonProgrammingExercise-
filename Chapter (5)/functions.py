aa9103ed4eb465313390c51a6e9260841a59184b
Zhao Xin <7176466@qq.com>
2020-05-20 23:08:34 +0800

    没空格怎么split呢

--- Chapter (5)/functions.py
+++ Chapter (5)/functions.py
@@ -1,4 +1,4 @@
-# _*_ coding:utf-8 _*_ 
+# _*_ coding:utf-8 _*_
 # Chapter 5
 from __future__ import division
 import math
@@ -19,7 +19,7 @@ def judge_year(year):
         print "Leap Year! "
     else:
         return "Wrong! "
-        
+
 
 # 5-5
 def change():
@@ -28,16 +28,16 @@ def change():
     b,money = divmod(money,10)
     c,money = divmod(money,5)
     d,money = divmod(money,1)
-    
+
     return """
     We have %s dollars in all.
     %d 25cents,%d 10cents,%d 5cents,%d 1cents.
     """ % (round((moneyamount / 100),2),a,b,c,d)
-    
 
-# 5-6    
+
+# 5-6
 def calculate(expression):
-    expression = raw_input("Please enter the expression (example:1+2) : ")
+    expression = raw_input("Please enter the expression (example:1 + 2) : ")
     elements = expression.split()
     N1 = float(elements[0])
     N2 = float(elements[2])
@@ -56,15 +56,15 @@ def calculate(expression):
         return N1 % N2
     else:
         print "Unknown expression! "
-     
- 
+
+
 # 5-7
 def tax():
     price = random.randrange(0,100)
     tax = 0.05
     return price*tax
-    
-    
+
+
 # 5-8
 def geometry_calculate():
     length = int(raw_input("Enter the length: "))
@@ -91,16 +91,16 @@ def f_to_c(f):
 def c_to_f(c):
     f = c / (5. / 9.) + 32
     return f
-    
 
-# 5-11    
+
+# 5-11
 def resides():
     for i in range(0,21):
         if i % 2 != 0:
             print "Odd number: ",i
         elif i % 2 == 0:
             print "Even number: ",i
-            
+
 def residues_two():
     N1 = int(raw_input("the first number: "))
     N2 = int(raw_input("the second number: "))
@@ -109,7 +109,7 @@ def residues_two():
     else:
         return False
 
-        
+
 # 5-12
 def check():
     i = 99999999999
@@ -123,15 +123,15 @@ def change_time():
     minute = int(raw_input("Enter minutes amount: "))
     minutes = hour * 60 + minute
     return minutes
-    
-    
+
+
 # 5-14
 def bank_tax(money):
     money = int(money)
     rate = 0.3
     FV = money*(1 + rate) ** 1
     return FV
-    
+
 
 # 5-15
 def divisor_and_multiple():
@@ -141,20 +141,20 @@ def divisor_and_multiple():
     for num in range(1,N1) and range(1,N2):
         if N1 % num == 0 and N2 % num == 0:
             divisor.append(num)
-            
-    # calculate the multiple of bigger one,then find the common multiple.     
+
+    # calculate the multiple of bigger one,then find the common multiple.
     big = max(N1,N2)
     N3 = (big * n for n in range(1,min(N1,N2)+1))
     multiple = []
     for num in N3:
         if num % min(N1,N2) == 0:
             multiple.append(num)
-    
+
     return """
     Maximum divisor is:%d
     Minimum multiple is:%d""" % (max(divisor),min(multiple))
-    
-    
+
+
 # 5-16
 def house_finance():
     open = float(raw_input("Enter opening balance: "))
@@ -170,26 +170,26 @@ def house_finance():
         Pymt += 1
     # Still a bit of problem...
 
-# 5-17       
+# 5-17
 def random_number():
     randomNum = random.randint(2,101)
     Num = 0
     numbers = []
-    
+
     while Num < randomNum:
         num = random.randint(0,2**31)
         numbers.append(num)
         Num += 1
-    
+
     length = random.randint(1,101)
     NewNum = []
-    
+
     a = 0
     while a < length:
         num = random.choice(numbers) # get random number
         NewNum.append(num)
         a += 1
-        
+
     finalNum = sorted(NewNum)
     return finalNum
 
